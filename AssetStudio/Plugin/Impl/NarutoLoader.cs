f7206e57c678a6b66ec0235153b934dc08cd4c40
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-10-05 16:10:07 +0200

    Add new loader

---
+++ AssetStudio/Plugin/Impl/NarutoLoader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace AssetStudio.Plugin.Impl;
+
+public class NarutoLoader : IFileLoader
+{
+    public Stream ProcessFile(Stream file, string filename)
+    {
+        file.Seek(9, SeekOrigin.Current);
+        var headerData = (stackalloc byte[0x1f]);
+        file.CheckedRead(headerData);
+
+        var blocksSizeBytes = (stackalloc byte[0xc]);
+        file.CheckedRead(blocksSizeBytes);
+
+        var blocksSize = blocksSizeBytes[3] | (blocksSizeBytes[2] << 8) | (blocksSizeBytes[1] << 16) | (blocksSizeBytes[0] << 24);
+
+        file.Seek(0xc, SeekOrigin.Current);
+
+        var blocks = new byte[blocksSize];
+        file.CheckedRead(blocks);
+
+        var key = "X@85Pq!6v$lCt7UYsihH3!cPb1P71bo4lX59FXqY!VO$YiYsu!Keu3aVZwi5on5l"u8;
+
+        var encSpan = blocks.AsSpan();
+
+        var ulongCount = blocks.Length / 8;
+        for (int i = 0; i < ulongCount; i++)
+        {
+            MemoryMarshal.Cast<byte, ulong>(encSpan)[i] ^= MemoryMarshal.Cast<byte, ulong>(key)[i % 8];
+        }
+
+        var rem = blocks.Length - ulongCount * 8;
+        if (rem > 0)
+        {
+            for (int i = ulongCount * 8; i < blocks.Length; i++)
+            {
+                encSpan[i] ^= key[i % 0x40];
+            }
+        }
+
+        var zeroSpan = (stackalloc byte[0xe]);
+        zeroSpan.Clear();
+
+        var ms = new MemoryStream();
+        ms.Write("UnityFS"u8);
+        ms.Write(headerData);
+        ms.Write(blocksSizeBytes);
+        ms.Write(zeroSpan);
+        ms.Write(encSpan);
+        file.CopyTo(ms);
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        return ms;
+    }
+
+    public bool CanProcessFile(Stream file, string filename)
+    {
+        var reader = new EndianBinaryReader(file);
+        return reader.ReadStringToNull(9) == "UnityKHFS";
+    }
+}
+
+public static class Extensions 
+{
+    public static bool CheckedRead(this Stream stream, Span<byte> data)
+    {
+        var read = stream.Read(data);
+        Debug.Assert(read == data.Length, "read == data.Length");
+        return read == data.Length;
+    }
+
+    public static bool CheckedRead(this Stream stream, byte[] data)
+    {
+        var read = stream.Read(data);
+        Debug.Assert(read == data.Length, "read == data.Length");
+        return read == data.Length;
+    }
+}
