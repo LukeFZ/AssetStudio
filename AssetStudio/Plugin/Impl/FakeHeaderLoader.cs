130368409dcf91a61cb888949f6b8869cc6a2354
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-12-04 17:17:24 +0100

    Add generalized fake header loader

---
+++ AssetStudio/Plugin/Impl/FakeHeaderLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+
+namespace AssetStudio.Plugin.Impl;
+
+// JewelPriLoader
+public class FakeHeaderLoader : IFileLoader
+{
+    public Stream ProcessFile(Stream file, string filename)
+    {
+        var offset = FindOffset(file);
+        return new OffsetStream(file, offset);
+    }
+
+    public bool CanProcessFile(Stream file, string filename)
+    {
+        if (file.Length < 8) return false;
+        return FindOffset(file) != -1;
+    }
+
+    private static int FindOffset(Stream file)
+    {
+        var buf = ArrayPool<byte>.Shared.Rent(0x250);
+
+        file.Position = 1;
+        var read = file.Read(buf, 0, 0x250);
+        var offset = buf.AsSpan(0, read).LastIndexOf("UnityFS"u8);
+
+        ArrayPool<byte>.Shared.Return(buf);
+
+        return offset == -1 ? offset : offset + 1;
+    }
+}
---
+++ AssetStudio/Plugin/OffsetStream.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace AssetStudio.Plugin;
+
+public class OffsetStream : Stream
+{
+    private readonly Stream _stream;
+    private readonly long _offset;
+
+    public OffsetStream(Stream stream, int offset = -1)
+    {
+        _stream = stream;
+
+        if (offset != -1)
+            _stream.Position = offset;
+
+        _offset = _stream.Position;
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+        => _stream.Read(buffer, offset, count);
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return origin == SeekOrigin.Begin 
+            ? _stream.Seek(_offset + offset, origin) 
+            : _stream.Seek(offset, origin);
+    }
+
+    public override void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanRead => _stream.CanRead;
+    public override bool CanSeek => _stream.CanSeek;
+    public override bool CanWrite => false;
+    public override long Length => _stream.Length - _offset;
+    public override long Position
+    {
+        get => _stream.Position - _offset;
+        set => _stream.Position = value + _offset;
+    }
+}
