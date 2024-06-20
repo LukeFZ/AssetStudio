1c4f482e8dd1967dd61e2edcf6f88d5a95b79da8
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2024-06-20 15:33:02 +0200

    update naruto loader

--- AssetStudio/Plugin/Impl/NarutoLoader.cs
+++ AssetStudio/Plugin/Impl/NarutoLoader.cs
@@ -8,7 +8,11 @@ public class NarutoLoader : FileLoader
 {
     public override Stream ProcessFile(Stream file, string filename)
     {
-        file.Seek(9, SeekOrigin.Current);
+        var reader = new EndianBinaryReader(file);
+        var magic = reader.ReadStringToNull(11);
+        var isOldFormat = magic == "UnityKHFS";
+        file.Seek(-1, SeekOrigin.Current);
+
         var headerData = (stackalloc byte[0x1f]);
         file.CheckedRead(headerData);
 
@@ -17,27 +21,48 @@ public class NarutoLoader : FileLoader
 
         var blocksSize = BinaryPrimitives.ReadUInt32BigEndian(blocksSizeBytes);
 
-        file.Seek(0xc, SeekOrigin.Current);
+        file.Seek(isOldFormat ? 0xc : 0xb, SeekOrigin.Current);
 
         var blocks = new byte[blocksSize];
         file.CheckedRead(blocks);
 
-        var key = "X@85Pq!6v$lCt7UYsihH3!cPb1P71bo4lX59FXqY!VO$YiYsu!Keu3aVZwi5on5l"u8;
+        var key = isOldFormat
+            ? "X@85Pq!6v$lCt7UYsihH3!cPb1P71bo4lX59FXqY!VO$YiYsu!Keu3aVZwi5on5l"u8 
+            : "hAi5luE8FlyblDdCTQC9uxnj3rkNwd1swrKI7Mx1aDFEe2B5h#3X&s54%GuSeHf@"u8; // UnityKHNFS
 
         var encSpan = blocks.AsSpan();
 
         var ulongCount = blocks.Length / 8;
+        var rem = blocks.Length - ulongCount * 8;
         for (int i = 0; i < ulongCount; i++)
         {
-            encSpan.As<ulong>()[i] ^= key.As<ulong>()[i % 8];
+            encSpan.As<ulong>()[i] ^= key.As<ulong>()[i % (key.Length / 8)];
         }
 
-        var rem = blocks.Length - ulongCount * 8;
         if (rem > 0)
         {
             for (int i = ulongCount * 8; i < blocks.Length; i++)
             {
-                encSpan[i] ^= key[i % 0x40];
+                encSpan[i] ^= key[i % key.Length];
+            }
+        }
+
+        if (!isOldFormat)
+        {
+            var secondKey = (stackalloc byte[8]);
+            BinaryPrimitives.WriteUInt64BigEndian(secondKey, blocksSize);
+
+            for (int i = 0; i < ulongCount; i++)
+            {
+                encSpan.As<ulong>()[i] ^= secondKey.As<ulong>()[0];
+            }
+
+            if (rem > 0)
+            {
+                for (int i = ulongCount * 8; i < blocks.Length; i++)
+                {
+                    encSpan[i] ^= secondKey[i % secondKey.Length];
+                }
             }
         }
 
@@ -60,6 +85,7 @@ public class NarutoLoader : FileLoader
     public override bool CanProcessFile(Stream file, string filename)
     {
         var reader = new EndianBinaryReader(file);
-        return reader.ReadStringToNull(9) == "UnityKHFS";
+        var magic = reader.ReadStringToNull(10);
+        return magic is "UnityKHFS" or "UnityKHNFS";
     }
 }
